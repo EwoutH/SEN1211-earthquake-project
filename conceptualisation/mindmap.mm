5fa0c85132175a5db20e0bb677c752acdd2ad41d
thomassandbergen <thomassandbergen@gmail.com>
2022-11-24 16:41:27 +0100

    First step conceptualisation
    Mindmap and some concepts

---
+++ conceptualisation/mindmap.mm
@@ -0,0 +1,102 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Earthquake response Turin" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1669302338156" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Agent" POSITION="right" ID="ID_1375082767" CREATED="1669302341311" MODIFIED="1669302358601">
+<edge COLOR="#ff0000"/>
+<node TEXT="Ambulances" ID="ID_1993477907" CREATED="1669302384783" MODIFIED="1669302484448">
+<node TEXT="Route information" ID="ID_872493493" CREATED="1669303299144" MODIFIED="1669303318344"/>
+</node>
+</node>
+<node TEXT="Objects" POSITION="right" ID="ID_1651891744" CREATED="1669302572438" MODIFIED="1669302576545">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Hospitals" ID="ID_322052832" CREATED="1669302580423" MODIFIED="1669302583843">
+<node TEXT="Capacity" ID="ID_1040944090" CREATED="1669302595879" MODIFIED="1669302599379"/>
+<node TEXT="Allows curing" ID="ID_517730495" CREATED="1669302600463" MODIFIED="1669302623514"/>
+<node TEXT="Location" ID="ID_1623101994" CREATED="1669302624431" MODIFIED="1669302632483"/>
+<node TEXT="Intake throughput" ID="ID_221770988" CREATED="1669303138527" MODIFIED="1669303158944"/>
+</node>
+<node TEXT="Building" ID="ID_524951981" CREATED="1669302584943" MODIFIED="1669302662745">
+<node TEXT="Construction material" ID="ID_136464502" CREATED="1669302663344" MODIFIED="1669302722097"/>
+<node TEXT="Size" ID="ID_504695192" CREATED="1669302725592" MODIFIED="1669302746609"/>
+<node TEXT="Damage status" ID="ID_1597632595" CREATED="1669302748884" MODIFIED="1669302753259"/>
+<node TEXT="Causes road closure" ID="ID_598519892" CREATED="1669302753991" MODIFIED="1669302798947"/>
+</node>
+<node TEXT="Road" ID="ID_1732710451" CREATED="1669302836535" MODIFIED="1669302841696">
+<node TEXT="Availibility" ID="ID_497453035" CREATED="1669302842204" MODIFIED="1669302851736"/>
+</node>
+<node TEXT="Crossings" ID="ID_1207238293" CREATED="1669302942631" MODIFIED="1669302947419"/>
+<node TEXT="Residents" ID="ID_240365686" CREATED="1669302361399" MODIFIED="1669302374048">
+<node TEXT="Degree injury" ID="ID_193321135" CREATED="1669302934391" MODIFIED="1669302941602"/>
+<node TEXT="(Disaster preparedness)" ID="ID_1489870583" CREATED="1669302949143" MODIFIED="1669302959752"/>
+</node>
+</node>
+</node>
+</map>
