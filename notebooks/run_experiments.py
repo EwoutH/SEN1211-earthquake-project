5b05c197ecc8ead3ecddaf4166b745b5113171f6
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2023-01-27 11:24:19 +0100

    Modify run_experiments for scenario testing
    7 scenarios added. Change exp_nr to modify the scenario ran.
    
    Results will be stored in results/experiments

--- notebooks/run_experiments.py
+++ notebooks/run_experiments.py
@@ -5,36 +5,40 @@ import pandas as pd
 os.environ["JAVA_HOME"] = 'C:/Program Files/NetLogo 6.3.0/runtime/bin/server/'
 
 default_values = {
-    # variable-name: (number, rounded to int or not)
-    "call-limit?": (True, False),
-    "earthquake-magnitude": (0.4, False),
-    "amount-ambulances": (40, True),
-    "amount-drones": (10, True),
-    "probability-call-112": (1, False),
-    "amount-hospitals": (10, True),
-    "hospital-capacity": (100, True),
-    "hospital-filling-percentage-t0": (60, False),
-    "initial-ambulance-search-radius": (5, True),
-    "percentage-concrete-buildings": (70, False),
-    "high-damage-road-blocked-chance": (10, False),
-    "collapsed-road-blocked-chance": (25, False),
-    "max-concurrent-calls": (50, True),
-    "average-call-time": (2.5, False),
-    "drone-speed": (0.5, False),
-    "drone-range": (45, False),
-    "ambulance-reroute-frequency": (5, True),
+    "call-limit?": True,
+    "earthquake-magnitude": 0.4,
+    "amount-ambulances": 40,
+    "amount-drones": 10,
+    "probability-call-112": 1,
+    "amount-hospitals": 10,
+    "hospital-capacity": 100,
+    "hospital-filling-percentage-t0": 60,
+    "initial-ambulance-search-radius": 5,
+    "percentage-concrete-buildings": 70,
+    "high-damage-road-blocked-chance": 10,
+    "collapsed-road-blocked-chance": 25,
+    "max-concurrent-calls": 50,
+    "average-call-time": 2.5,
+    "amount-drones": 10,
+    "drone-speed": 0.5,
+    "drone-range": 45,
+    "ambulance-reroute-frequency": 5,
+    "drone-view-radius": 250,
 }
 
-replications = 25
-ticks = 720
-value_to_vary = list(default_values.keys())[14]  # Change this one from 1 to 17 to quickly alter the value varried
-amount_to_vary = [0.8, 1.25]
-
-factor = True      # Set false if you want to do custom values, and for the call-limit (True / False)
-fixed_values = []  # Put custom values here
+replications = 5
+ticks = 120
 gui = False
 
-value_to_vary_name = value_to_vary.replace("?", "")  # File names can't contain question marks
+exp_nr = 2
+exp_names = ["no-drones", "short-range", "slow-reroute", "more-drones", "large-view", "fast-reroute", "many-long-range"]
+exp_name = f"{exp_nr}_{exp_names[exp_nr]}"
+exp = {
+    "amount-drones": [0, 10, 10, 20, 20, 20, 40],
+    "drone-view-radius": [250, 250, 250, 250, 500, 500, 500],
+    "drone-range": [30, 30, 45, 45, 45, 45, 60],
+    "ambulance-reroute-frequency": [10, 10, 10, 10, 10, 5, 5],
+}
 
 series_reporters = [
     "recovered-hospital",
@@ -58,58 +62,43 @@ netlogo.load_model("C:/Users/Ewout/Documents/GitHub/SEN1211-earthquake-project/m
 single_data = {}
 series_data = {}
 
-if factor:
-    values = [round(default_values[value_to_vary][0] * v, 5) for v in amount_to_vary]
-else:
-    values = fixed_values
+print(f"Starting experiment {exp_name} with {replications} runs.")
+for var, val in exp.items():
+    print(f"Using {var} = {val[exp_nr]}")
+print("")
 
-if default_values[value_to_vary][1]:
-    print(f"Rounding values to integers: {values}")
-    values = [int(v) for v in values]
-    print(f"Now rounded: {values}\n")
+for i in range(replications):
+    # Setup model
+    netlogo.command("setup")
+    for var, val in exp.items():
+        netlogo.command(f"set {var} {val[exp_nr]}")
 
-runs = len(values) * replications
-print(f"Starting {len(values)} * {replications} = {runs} runs.")
+    # Record initial data and run
+    single_data[i] = netlogo.report(single_reporters)
+    series_data[i] = netlogo.repeat_report(series_reporters, ticks)
 
-for v in values:
-    print(f"Start with {replications} replications with {value_to_vary} = {v}.")
-    single_data[v] = {}
-    series_data[v] = {}
-
-    for i in range(replications):
-        # Setup model
-        netlogo.command("setup")
-        netlogo.command(f"set {value_to_vary} {v}")
-
-        # Record initial data and run
-        single_data[v][i] = netlogo.report(single_reporters)
-        series_data[v][i] = netlogo.repeat_report(series_reporters, ticks)
-
-        print(f"Finished run {i+1} of {replications}.")
-
-    # Combine the series_data to a dataframe and save it
-    # Create a list of tuples with the key and dataframe
-    dfs = [(k, df) for k, df in series_data[v].items()]
-
-    # Concatenate the DataFrames in the dictionary along axis=1
-    result = pd.concat([df for key, df in dfs], keys=[key for key, df in dfs], axis=1)
-
-    # Reorder the levels and sort
-    result.columns = result.columns.reorder_levels([1, 0])
-
-    # Save df as pickle
-    result.to_pickle(f"../results/sensitivity/sens_series_{value_to_vary_name}_{v}_{replications}r_df.pickle")
+    print(f"Finished run {i+1} of {replications}.")
 
 netlogo.kill_workspace()
 
+# Combine the series_data to a dataframe and save it
+# Create a list of tuples with the key and dataframe
+dfs = [(k, df) for k, df in series_data.items()]
+
+# Concatenate the DataFrames in the dictionary along axis=1
+result = pd.concat([df for key, df in dfs], keys=[key for key, df in dfs], axis=1)
+
+# Reorder the levels and sort
+result.columns = result.columns.reorder_levels([1, 0])
+
+# Save df as pickle
+result.to_pickle(f"../results/experiments/exp_series_{exp_name}_{replications}r_df.pickle")
+
+
 # Combine single_data to a DataFrame
 sdf = pd.DataFrame(single_data)
-sdf_dict = {}
-
-for column in sdf.columns:
-    sdf_dict[column] = pd.DataFrame(sdf[column].to_list(), columns=single_reporters)
-sdf2 = pd.concat(sdf_dict.values(), axis=1, keys=sdf_dict.keys())
-
-sdf2.to_pickle(f"../results/sensitivity/sens_single_{value_to_vary_name}_{replications}r_df.pickle")
+sdf = sdf.T
+sdf.columns = single_reporters
+sdf.to_pickle(f"../results/experiments/exp_single_{exp_name}_{replications}r_df.pickle")
 
 print("Done. Results are saved in results/sensitivity.")
